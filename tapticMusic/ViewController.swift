9e18b9dc04a39905b19710fd462e6a82e1b1fd9b
wbaumgardner <wbaumgardner@ucdavis.edu>
2020-02-04 21:12:06 -0800

    Initial Commit

---
+++ tapticMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tapticMusic
+//
+//  Created by William Baumgardner on 2/4/20.
+//  Copyright © 2020 William Baumgardner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
